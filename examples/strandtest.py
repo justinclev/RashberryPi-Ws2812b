ba27d52a49d11336821295e757219f9df1b00ad5
Justin Clevenger <justinclev@Justins-MacBook-Pro.local>
2020-02-25 22:34:00 -0500

    Create Strip Object

--- examples/strandtest.py
+++ examples/strandtest.py
@@ -9,16 +9,8 @@ import time
 from neopixel import *
 import argparse
 
-# LED strip configuration:
-LED_COUNT      = 16      # Number of LED pixels.
-LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-
+class Strip(object):
+    pass
 
 
 # Define functions which animate LEDs in various ways.
@@ -93,10 +85,19 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
     args = parser.parse_args()
 
+    strip = Strip()
+    strip.Count = 5
+    strip.Pin = 18
+    strip.Freq_Hz = 800000
+    strip.DMA = 255
+    strip.Brightness = 255
+    strip.Invert = False
+    strip.Channel = 0
+
     # Create NeoPixel object with appropriate configuration.
-    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    strip.leds = Adafruit_NeoPixel(strip.Count, strip.Pin, strip.Freq_Hz, strip.DMA, strip.Invert, strip.Brightness, strip.Channel)
     # Intialize the library (must be called once before other functions).
-    strip.begin()
+    strip.leds.begin()
 
     print ('Press Ctrl-C to quit.')
     if not args.clear:
@@ -106,7 +107,7 @@ if __name__ == '__main__':
 
         while True:
             print ('Color wipe animations.')
-            colorFlash(strip, Color(255, 0, 0)) # Red Flash
+            colorFlash(strip.leds, Color(255, 0, 0)) # Red Flash
             # colorWipe(strip, Color(255, 0, 0))  # Red wipe
             # colorWipe(strip, Color(0, 255, 0))  # Blue wipe
             # colorWipe(strip, Color(0, 0, 255))  # Green wipe
@@ -121,4 +122,4 @@ if __name__ == '__main__':
 
     except KeyboardInterrupt:
         if args.clear:
-            colorWipe(strip, Color(0,0,0), 10)
+            colorWipe(strip.leds, Color(0,0,0), 10)
