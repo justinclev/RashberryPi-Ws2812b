e894c5af4cbab2bff1e5592b884b213cf01fd414
Justin Clevenger <justinclev@Justins-MacBook-Pro.local>
2020-02-26 00:13:36 -0500

    revert

--- examples/strandtest.py
+++ examples/strandtest.py
@@ -9,27 +9,19 @@ import time
 from neopixel import *
 import argparse
 
-class Strip(object):
-    def __init__(self, count, pin):  
-        self.count = count
-        self.pin = pin
-        self.freq_hz = 800000
-        self.dma = 255
-        self.brightness = 255
-        self.invert = False
-        self.channel = 0
-        # self.leds = Adafruit_NeoPixel(self.count, self.pin, self.freq_hz, self.dma, self.invert, self.brightness, self.channel)
-        
+# LED strip configuration:
+LED_COUNT      = 16      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+
 
 # Define functions which animate LEDs in various ways.
-def colorFlash(strip, color, wait_ms=50):
-    """Flash color accross strip"""
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, color)
-
-    strip.show()
-    time.sleep(wait_ms/1000.0)
-
 def colorWipe(strip, color, wait_ms=50):
     """Wipe color across display a pixel at a time."""
     for i in range(strip.numPixels()):
@@ -93,12 +85,10 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
     args = parser.parse_args()
 
-    strip = Strip(5, 18)
-    leds = Adafruit_NeoPixel(strip.count, strip.pin, strip.freq_hz, strip.dma, strip.invert, strip.brightness, strip.channel)
-       
-    # for strip in strips:
+    # Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     # Intialize the library (must be called once before other functions).
-    leds.begin()
+    strip.begin()
 
     print ('Press Ctrl-C to quit.')
     if not args.clear:
@@ -108,19 +98,18 @@ if __name__ == '__main__':
 
         while True:
             print ('Color wipe animations.')
-            colorFlash(leds, Color(255, 0, 0)) # Red Flash
-            # colorWipe(strip, Color(255, 0, 0))  # Red wipe
-            # colorWipe(strip, Color(0, 255, 0))  # Blue wipe
-            # colorWipe(strip, Color(0, 0, 255))  # Green wipe
-            # print ('Theater chase animations.')
-            # theaterChase(strip, Color(127, 127, 127))  # White theater chase
-            # theaterChase(strip, Color(127,   0,   0))  # Red theater chase
-            # theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
-            # print ('Rainbow animations.')
-            # rainbow(strip)
-            # rainbowCycle(strip)
-            # theaterChaseRainbow(strip)
+            colorWipe(strip, Color(255, 0, 0))  # Red wipe
+            colorWipe(strip, Color(0, 255, 0))  # Blue wipe
+            colorWipe(strip, Color(0, 0, 255))  # Green wipe
+            print ('Theater chase animations.')
+            theaterChase(strip, Color(127, 127, 127))  # White theater chase
+            theaterChase(strip, Color(127,   0,   0))  # Red theater chase
+            theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
+            print ('Rainbow animations.')
+            rainbow(strip)
+            rainbowCycle(strip)
+            theaterChaseRainbow(strip)
 
     except KeyboardInterrupt:
         if args.clear:
-            colorWipe(leds, Color(0,0,0), 10)
+            colorWipe(strip, Color(0,0,0), 10)
